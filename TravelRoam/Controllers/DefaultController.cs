6534c01f98561574652c7c92e77add62666d2551
Mertisk <merrtt.isk@gmail.com>
2024-04-15 12:43:16 +0300

    Add project files.

---
+++ TravelRoam/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelRoam.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ TravelRoam/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TravelRoam.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
